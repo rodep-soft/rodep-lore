d7cbba9e3da966e1e10ff04ec000857859b42c3a
lazytatzv <yano.tatsuki439@mail.kyutech.jp>
2025-12-26 16:20:09 +0900

    add servo code

---
+++ estop_heater/src/main.cpp
@@ -0,0 +1,36 @@
+#include <Arduino.h>
+#include <Servo.h>
+
+Servo servo;
+
+const int servo_pin = 9;
+int incomingByte = 0;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Start program"); 
+
+  servo.attach(servo_pin);
+
+  if (!servo.attached()) {
+    Serial.println("Servo not attached!");
+  }
+}
+
+void loop() {
+
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    incomingByte = Serial.read();
+
+    Serial.println("I received:");
+    Serial.println(incomingByte, DEC);
+
+    if (DEC == 1) {
+      servo.write(90);
+    }
+  }
+  delay(20);
+} 
+
+
